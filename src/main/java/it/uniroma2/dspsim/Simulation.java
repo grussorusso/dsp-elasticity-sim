22b8cd7f93e3d3867f79b492ec0cf99e535634f6
Zed25 <2simonemancini5@gmail.com>
2020-02-01 12:42:44 +0100

    Deep learning OM learning phase little problem fixed

--- src/main/java/it/uniroma2/dspsim/Simulation.java
+++ src/main/java/it/uniroma2/dspsim/Simulation.java
@@ -273,7 +273,7 @@ public class Simulation {
 					.getString(ConfigurationKeys.INPUT_FILE_PATH_KEY, "/home/gabriele/profile.dat");
 			InputRateFileReader inputRateFileReader = new InputRateFileReader(inputFile);
 
-			Application app = ApplicationBuilder.defaultApplication();
+			Application app = ApplicationBuilder.singleOperatorApplication();
 			ApplicationManager am = new ApplicationManager(app);
 
 			Simulation simulation = new Simulation(inputRateFileReader, am);
