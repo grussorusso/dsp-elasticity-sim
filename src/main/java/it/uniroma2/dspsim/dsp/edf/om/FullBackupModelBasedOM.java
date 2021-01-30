e44ad1a37469ce38d2d80f5dae695b1fdbf5ad49
Gabriele Russo Russo <gabri.russo17@gmail.com>
2021-01-30 23:24:33 +0100

    Model-based initialized w approximate model

--- src/main/java/it/uniroma2/dspsim/dsp/edf/om/FullBackupModelBasedOM.java
+++ src/main/java/it/uniroma2/dspsim/dsp/edf/om/FullBackupModelBasedOM.java
@@ -14,12 +14,20 @@ import it.uniroma2.dspsim.dsp.edf.om.rl.states.StateType;
 import it.uniroma2.dspsim.dsp.edf.om.rl.utils.ActionIterator;
 import it.uniroma2.dspsim.dsp.edf.om.rl.utils.StateIterator;
 import it.uniroma2.dspsim.dsp.edf.om.rl.utils.StateUtils;
+import it.uniroma2.dspsim.dsp.queueing.MG1OperatorQueueModel;
+import it.uniroma2.dspsim.dsp.queueing.OperatorQueueModel;
 import it.uniroma2.dspsim.infrastructure.ComputingInfrastructure;
 import it.uniroma2.dspsim.stats.Statistics;
 import it.uniroma2.dspsim.utils.matrix.DoubleMatrix;
 import it.uniroma2.dspsim.utils.parameter.VariableParameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
 
 public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 
@@ -31,15 +39,19 @@ public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 
 	private double gamma;
 
-	private int offlineObservationsParam = 400000;
+	// TODO: restore contraints in the learning step
+	private boolean initWithApproximateModel = true;
+	private int offlineObservationsParam = 10000;
 
 	private VariableParameter alpha;
 	private int alphaDecaySteps;
 	private int alphaDecayStepsCounter;
 
-	private final int skipFullBackupAfter = 10000;
-	private final int fullBackupEvery = 50;
+	private final int skipFullBackupAfter = 100000;
+	private final int fullBackupEvery = 100;
 	private int time = 0;
+
+	private int onlineVIMaxIter = 3; // TODO configurable
 
 	private Logger logger = LoggerFactory.getLogger(FullBackupModelBasedOM.class);
 
@@ -69,14 +81,33 @@ public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 		logger.info("Alpha conf: init={}, decay={}, currValue={}", alphaInitValue,
 				alphaDecay, alpha.getValue());
 
-		if (offlineObservationsParam > 0) {
+		if (initWithApproximateModel) {
 			offlinePlanning();
+		} else {
+			offlineObservationsParam = 0;
 		}
 
 	}
 
+	private Operator approximateOperatorModel (Operator op)
+	{
+		Random r = new Random(123); // TODO seed
+		//final double maxErr = 0.2;
+		//final double err = maxErr * r.nextDouble() -maxErr/2.0;
+		//final double stMean = op.getQueueModel().getServiceTimeMean() * (1.0 + err);
+		//final double stVar = Math.pow(stMean,2);
+
+		//logger.info("Approximate stMean: {} -> {}", op.getQueueModel().getServiceTimeMean(), stMean);
+		//OperatorQueueModel queueModel = new MG1OperatorQueueModel(stMean, stVar);
+		OperatorQueueModel queueModel = op.getQueueModel().getApproximateModel(r);
+		Operator tempOperator = new Operator("temp", queueModel, op.getMaxParallelism());
+		tempOperator.setSloRespTime(op.getSloRespTime());
+		return tempOperator;
+	}
+
 	private void offlinePlanning() {
-		ValueIterationOM viOM = new ValueIterationOM(operator); // TODO we should use a different "wrong" operator model!
+		ValueIterationOM viOM = new ValueIterationOM(approximateOperatorModel(operator));
+		//ValueIterationOM viOM = new ValueIterationOM(operator);
 		QTable viQ = viOM.getQTable();
 		DoubleMatrix<Integer,Integer> viP = viOM.getPMatrix();
 
@@ -100,6 +131,7 @@ public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 				qTable.setQ(s, action, oldQ);
 			}
 		}
+		//dumpEstimatedCost();
 
 		/* Update transition model */
 		for (int startState = 0; startState < getInputRateLevels(); startState++) {
@@ -199,34 +231,37 @@ public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 		final int k = currentState.overallParallelism();
 		final double oldval = unknownCostEst[k-1][jLambda];
 		final double newval = (1.0-alpha.getValue())*oldval + alpha.getValue() * unknownCost;
+		//final double newval = (oldval * (offlineObservationsParam + time - 1) + unknownCost) / (offlineObservationsParam + time);
+		//if (newval != newval2)
+		//	logger.info("Diff: {} - {}", newval, newval2);
 		unknownCostEst[k-1][jLambda] = newval;
 
-		int k0,k1;
-		int l0,l1;
+		//int k0,k1;
+		//int l0,l1;
 
-		if (newval > oldval) {
-			k0 = 1;
-			k1 = k;
-			l0 = jLambda;
-			l1 = currentState.getMaxLambda();
-			for (int l = l0; l<=l1; ++l) {
-				for (int _k = k0; _k<=k1; ++_k) {
-					if (unknownCostEst[_k-1][l] < newval)
-						unknownCostEst[_k-1][l] = newval;
-				}
-			}
-		} else if (newval < oldval) {
-			k0 = k;
-			k1 = currentState.getMaxParallelism();
-			l0 = 0;
-			l1 = jLambda;
-			for (int l = l0; l<=l1; ++l) {
-				for (int _k = k0; _k<=k1; ++_k) {
-					if (unknownCostEst[_k-1][l] > newval)
-						unknownCostEst[_k-1][l] = newval;
-				}
-			}
-		}
+		//if (newval > oldval) {
+		//	k0 = 1;
+		//	k1 = k;
+		//	l0 = jLambda;
+		//	l1 = currentState.getMaxLambda();
+		//	for (int l = l0; l<=l1; ++l) {
+		//		for (int _k = k0; _k<=k1; ++_k) {
+		//			if (unknownCostEst[_k-1][l] < newval)
+		//				unknownCostEst[_k-1][l] = newval;
+		//		}
+		//	}
+		//} else if (newval < oldval) {
+		//	k0 = k;
+		//	k1 = currentState.getMaxParallelism();
+		//	l0 = 0;
+		//	l1 = jLambda;
+		//	for (int l = l0; l<=l1; ++l) {
+		//		for (int _k = k0; _k<=k1; ++_k) {
+		//			if (unknownCostEst[_k-1][l] > newval)
+		//				unknownCostEst[_k-1][l] = newval;
+		//		}
+		//	}
+		//}
 
 		//System.out.println("----------");
 		//for (int _k = 1; _k<= operator.getMaxParallelism(); ++_k) {
@@ -249,6 +284,10 @@ public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 		/* Do a full backup */
 		if (time < skipFullBackupAfter || time % fullBackupEvery == 0)
 			this.fullBackup();
+
+		if (time == 399000)
+			dumpEstimatedCost();
+			//dumpQOnFile("/tmp/fbq");
 	}
 
 
@@ -259,17 +298,23 @@ public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 
 	private double fullBackup() {
 		double delta = 0.0;
+		for (int iter = 0; iter < onlineVIMaxIter; ++iter) {
+			delta = 0.0;
 
-		StateIterator stateIterator = new StateIterator(this.getStateRepresentation(), this.operator.getMaxParallelism(),
-				ComputingInfrastructure.getInfrastructure(), this.getInputRateLevels());
+			StateIterator stateIterator = new StateIterator(this.getStateRepresentation(), this.operator.getMaxParallelism(),
+					ComputingInfrastructure.getInfrastructure(), this.getInputRateLevels());
 
-		while (stateIterator.hasNext()) {
-			State state = stateIterator.next();
-			double newDelta = computeValueIteration(state);
-			delta = Math.max(delta, newDelta);
+			while (stateIterator.hasNext()) {
+				State state = stateIterator.next();
+				double newDelta = computeValueIteration(state);
+				delta = Math.max(delta, newDelta);
+			}
+
+			logger.info("Delta: " + delta);
+			if (delta < 0.0001)
+				break;
 		}
 
-		logger.info("Delta: " + delta);
 		return delta;
 	}
 
@@ -318,5 +363,46 @@ public class FullBackupModelBasedOM extends ReinforcementLearningOM {
 			cost += p * (pdCost + gamma * q);
 		}
 		return cost;
+	}
+
+	protected void dumpQOnFile(String filename) {
+		// create file
+		File file = new File(filename);
+		try {
+			if (!file.exists()) {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+			}
+			PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File(filename), true));
+			StateIterator stateIterator = new StateIterator(getStateRepresentation(), operator.getMaxParallelism(),
+					ComputingInfrastructure.getInfrastructure(), getInputRateLevels());
+			while (stateIterator.hasNext()) {
+				State s = stateIterator.next();
+				// print state line
+				printWriter.println(s.dump());
+				ActionIterator ait = new ActionIterator();
+				while (ait.hasNext()) {
+					Action a = ait.next();
+					double v = this.qTable.getQ(s, a);
+					if (s.validateAction(a)) {
+						printWriter.print(String.format("%s\t%f\n", a.dump(), v));
+					}
+				}
+			}
+			printWriter.flush();
+			printWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void dumpEstimatedCost () {
+		System.out.println("----------");
+		for (int _k = 1; _k<= operator.getMaxParallelism(); ++_k) {
+			for (int l = 0; l < getInputRateLevels(); ++l) {
+				System.out.print(String.format("%.2f\t",unknownCostEst[_k-1][l]));
+			}
+			System.out.println("");
+		}
 	}
 }
